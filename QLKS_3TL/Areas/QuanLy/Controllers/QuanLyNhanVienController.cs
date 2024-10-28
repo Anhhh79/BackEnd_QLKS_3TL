2e67b12899b0d715bc2230e14c3c7080c1f973a5
BaoAnh79 <nbanhcntt2211026@student.ctuet.edu.vn>
2024-10-28 23:14:03 +0700

    Đẩy thông tin nhân viên từ cơ sở dữ liệu lên view

--- QLKS_3TL/Areas/QuanLy/Controllers/QuanLyNhanVienController.cs
+++ QLKS_3TL/Areas/QuanLy/Controllers/QuanLyNhanVienController.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QLKS_3TL.Areas.QuanLy.Models;
+using QLKS_3TL.Data;
 
 namespace QLKS_3TL.Areas.QuanLy.Controllers
 {
     public class QuanLyNhanVienController : Controller
     {
+        // nhúng cơ sở dữ liệu
+        private readonly Qlks3tlContext db;
+
+        public QuanLyNhanVienController(Qlks3tlContext context) { db = context; }
+
         public IActionResult Index()
         {
-            return View();
+            // Lấy tất cả nhân viên và chuyển sang view model
+            var nhanViens = db.NhanViens.AsQueryable();
+            var result = db.NhanViens
+               .Select(p => new NhanVienViewModal
+               {
+                   MaNhanVien = p.MaNhanVien,
+                   AnhNhanVien = p.AnhNhanVien ?? "",
+                   HoTen = p.HoTen ?? "",
+                   Cccd = p.Cccd ?? "",
+                   SoDienThoai = p.SoDienThoai ?? "",
+                   DiaChi = p.DiaChi ?? "",
+                   ChucVu = p.ChucVu ?? "",
+                   LuongCoBan = p.LuongCoBan ?? 0m,
+                   GioiTinh = p.GioiTinh ?? ""
+               });
+            return View(result);
         }
     }
 }
---
+++ QLKS_3TL/Areas/QuanLy/Models/NhanVienViewModal.cs
@@ -0,0 +1,24 @@
+﻿namespace QLKS_3TL.Areas.QuanLy.Models
+{
+    public class NhanVienViewModal
+    {
+
+     public string MaNhanVien { get; set; } 
+
+    public string AnhNhanVien { get; set; }
+
+    public string HoTen { get; set; }
+
+    public string Cccd { get; set; }
+
+    public string SoDienThoai { get; set; }
+
+    public string DiaChi { get; set; }
+
+    public string ChucVu { get; set; }
+
+    public decimal LuongCoBan { get; set; }
+
+    public string GioiTinh { get; set; }
+    }
+}
