b9a6735e2b6da3df8c46d8cc0c3a7cb84b37d9e1
QuocBao <quocbao272004@gmail.com>
2024-12-03 11:31:26 +0700

    Quản lý khách hàng ở lễ tân

---
+++ QLKS_3TL/Areas/LeTan/Models/TTKHcs.cs
@@ -0,0 +1,19 @@
+﻿namespace QLKS_3TL.Areas.LeTan.Models
+{
+    public class TTKHcs
+    {
+        public string? SoDienThoai { get; set; }
+
+        public string? HoTen { get; set; }
+
+        public string? Cccd { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? GioiTinh { get; set; }
+
+        public string? MaKhachHang { get; set; }
+
+        public DateTime? ThoiGianDatPhong { get; set; }
+    }
+}
